4d428887e92cf7ecd65f47032fd01c29657ee158
lahla81 <lahla81@yahoo.com>
2019-09-29 17:34:47 +0200

    Updates

---
+++ precache-manifest.035db81101848983413754df5399d0cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df2c8aa9d9fc395481ac262e108c0fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81e52e0b242ac25f43e7",
+    "url": "/static/css/2.1e00258c.chunk.css"
+  },
+  {
+    "revision": "53d046271b125d8b7310",
+    "url": "/static/css/main.ffb42fb1.chunk.css"
+  },
+  {
+    "revision": "81e52e0b242ac25f43e7",
+    "url": "/static/js/2.521213d1.chunk.js"
+  },
+  {
+    "revision": "53d046271b125d8b7310",
+    "url": "/static/js/main.0a19947d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
